8f3f817c6a4dccde9dae41ed49c573c036fa129f
Farad Alam <farad.dev@gmail.com>
2026-09-24 10:06:36 +0600

    Remove obsolete cart drawer component from layout

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
-import CartDrawer from "@/components/cart/CartDrawer";
 
 export default function StoreLayout({
   children,
@@ -12,7 +11,6 @@ export default function StoreLayout({
       <Navbar />
       <main>{children}</main>
       <Footer />
-      <CartDrawer />
     </>
   );
 }
